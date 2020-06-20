0ace0d2da3246d097a2db585806f2d7e8430aa35
KubotaTatsuya <t.kubota.sys19@morijyobi.ac.jp>
2020-06-20 10:51:18 +0900

    commit

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 ?>
 <?php
 $dsn = 'mysql:dbname=sample_db;host=localhost;';
-$user = 'otsubo';
-$password = 'morijyobi';
+$user = 'kubota';
+$password = '4191121shiba@KTjp';
 try {
     $dbh = new PDO($dsn, $user, $password);
     $sql = "select * from sample; ";
@@ -15,3 +15,22 @@ try {
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LAMP TEST PAGE</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php foreach ($result as $value) { ?>
+            <?php echo "$value[name]"; ?>
+        <?php } ?>
+    </div>?>?
+</body>
+
+</html>
