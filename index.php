2999d79ff94dde0485285cada3eaf2a39f6b41d3
KubotaTatsuya <t.kubota.sys19@morijyobi.ac.jp>
2020-06-26 13:27:57 +0900

    commit

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ $user = 'kubota';
 $password = '4191121shiba@KTjp';
 try {
     $dbh = new PDO($dsn, $user, $password);
+    print "Success\n";
     $sql = "select * from user; ";
 	$result = $dbh->query($sql);
 } catch (PDOException $e) {
@@ -75,68 +76,46 @@ try {
                 </table>
             </div>
             <div class="tab-pane" id="insert">
-
-<form class="mt-3" action="./insert.php" method="POST">
-
-    <div class="form-group row">
-
-        <label for="id" class="col-sm-2 col-form-label">ID</label>
-
-        <div class="col-sm-10">
-
-            <input type="text" class="form-control" id="id" name="id">
-
-        </div>
-
-    </div>
-
-    <div class="form-group row">
-
-        <label for="name" class="col-sm-2 col-form-label">Your Name</label>
-
-        <div class="col-sm-10">
-
-            <input type="text" class="form-control" id="name" name="name">
-
-        </div>
-
-    </div>
-
-    <div class="form-group row">
-
-        <label for="age" class="col-sm-2 col-form-label">Age</label>
-
-        <div class="col-sm-10">
-
-            <input type="text" class="form-control" id="age" name="age">
-
-        </div>
-
-    </div>
-
-    <div class="form-group">
-
-        <button type="submit" class="btn btn-primary btn-block">Insert</button>
-
-    </div>
-
-</form>
-
-</div>    <div class="tab-pane" id="insert">
-                <form action="./insert.php" method="POST">
-                <div class="form-group row">
+            
+            <form class="mt-3" action="./insert.php" method="POST">
+					<div class="form-group row">
+						<label for="id" class="col-sm-2 col-form-label">ID</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="id" name="id">
+						</div>
+					</div>
+					<div class="form-group row">
+						<label for="name" class="col-sm-2 col-form-label">Your Name</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="name" name="name">
+						</div>
+					</div>
+					<div class="form-group row">
+						<label for="age" class="col-sm-2 col-form-label">Age</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="age" name="age">
+						</div>
+					</div>
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary btn-block">Insert</button>
+					</div>
+				</form>
+</div> 
+   <div class="tab-pane" id="insert">
+     <form action="./insert.php" method="POST">
+            <div class="form-group row">
                  <label for="id" class="col-sm-2 col-form-label">ID</label>
                 <div class="col-sm-10">
                      <input type="text" class="form-control" id="id" name="id">
                  </div>
              </div>
-                <div class="form-group row">
+            <div class="form-group row">
                  <label for="name" class="col-sm-2 col-form-label">Name</label>
-                <div class="col-sm-10">
-                     <input type="text" class="form-control" id="name" name="name">
-                 </div>
-             </div>
-            </form>
+            <div class="col-sm-10">
+                    <input type="text" class="form-control" id="name" name="name">
+            </div>
+        </div>
+    </form>
 
             </div>
             <div class="tab-pane" id="update">
--- insert.php
+++ insert.php
@@ -4,24 +4,22 @@ $user = 'kubota';
 $password = '4191121shiba@KTjp';
 try {
     $dbh = new PDO($dsn, $user, $password);
-    $dbh->set_attribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
     $id = $_POST['id'];
     $name = $_POST['name'];
     $age = $_POST['age'];
 
-
-    $sql = "insert into user values (:id, :name, :age)";
-    $stmt = $dbh->prepare($sql);
-    $prams = array(':id'=> $id, ':name'=> $name, ':age'=> $age,);
+    $sql = "insert into user values(:id, :name, :age)";
+    $stmt=$dbh->prepare($sql);
+    $prams=array(':id'=>$id,':name'=>$name,':age'=>$age,);
     $stmt->execute($prams);
 
     header('Location: index.php?fg=1');
 
 } catch (PDOException $e) {
-   header('Location: index.php?fg=2?err='.$e->getMassage());
+    print "Failed: " . $e->getMessage() . "\n";
     exit();
 }
-
 
 ?>
