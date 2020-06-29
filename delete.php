135f56fdf7ac58cfe0190c3fd222bd07d2da3ce9
KubotaTatsuya <t.kubota.sys19@morijyobi.ac.jp>
2020-06-29 13:46:58 +0900

    commit

--- delete.php
+++ delete.php
@@ -6,40 +6,21 @@ $user = 'kubota';
 
 $password = '4191121shiba@KTjp';
 
-
-
 try {
-
     $dbh = new PDO($dsn, $user, $password);
-
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
 
     $id = $_GET['id'];
 
-
-
     $sql = "delete from user where id=:id";
-
     $stmt = $dbh->prepare($sql);
-
     $prams = array(':id'=> $id);
-
     $stmt->execute($prams);
 
-
-
     header('Location: index.php?fg=1');
-
     
-
 } catch (PDOException $e) {
-
 	header('Location: index.php?fg=2?err='.$e->getMessage());
-
 	exit();
-
 }
-
 ?>
