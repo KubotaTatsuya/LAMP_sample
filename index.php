d36c5a3b364fd4990c11a3848129f5c919422fb8
KubotaTatsuya <t.kubota.sys19@morijyobi.ac.jp>
2020-06-29 11:37:53 +0900

    commit

--- index.php
+++ index.php
@@ -118,22 +118,55 @@ try {
     </form>
 
      </div>
-        <div class="tab-pane" id="update">
-            <form action="./update.php" method="POST">
-            <div class="form-group row">
-                 <label for="id" class="col-sm-2 col-form-label">ID</label>
-                <div class="col-sm-10">
-                     <input type="text" class="form-control" id="id" name="id">
-                 </div>
-             </div>
-            <div class="form-group row">
-                 <label for="name" class="col-sm-2 col-form-label">Name</label>
-            <div class="col-sm-10">
-                    <input type="text" class="form-control" id="name" name="name">
-            </div>
+     <div class="tab-pane" id="update">
+
+<form class="mt-3" action="./update.php" method="POST">
+
+    <div class="form-group row">
+
+        <label for="id" class="col-sm-2 col-form-label">ID</label>
+
+        <div class="col-sm-10">
+
+            <input type="text" class="form-control" id="id" name="id">
+
         </div>
-    </form>
-            </div>
+
+    </div>
+
+    <div class="form-group row">
+
+        <label for="name" class="col-sm-2 col-form-label">Your Name</label>
+
+        <div class="col-sm-10">
+
+            <input type="text" class="form-control" id="name" name="name">
+
+        </div>
+
+    </div>
+
+    <div class="form-group row">
+
+        <label for="age" class="col-sm-2 col-form-label">Age</label>
+
+        <div class="col-sm-10">
+
+            <input type="text" class="form-control" id="age" name="age">
+
+        </div>
+
+    </div>
+
+    <div class="form-group">
+
+        <button type="submit" class="btn btn-primary btn-block">Insert</button>
+
+    </div>
+
+</form>
+
+</div>
             <div class="tab-pane" id="delete">
                 <p>sample tab 4</p>
             </div>
